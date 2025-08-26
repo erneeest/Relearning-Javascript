5e435de39fbb8027403e36a3df05448101ea5176
Ernest Talagtag <ernestneil09@gmail.com>
2025-08-26 23:43:04 +0800

    tried DOM

---
+++ DocumentObjectModel/script.js
@@ -0,0 +1,12 @@
+const submitButton = document.querySelector('.submit-button').innerHTML;
+
+console.log(submitButton);
+
+// console.log(document.title);
+// document.title = 'changed';
+
+// console.log(document.body);
+// console.log(typeof document.body);
+
+// console.log(document.body.innerHTML);
+// document.body.innerHTML = '<h1> CEELINE-JAYNE LOVE OF MY LIFEE!!!!</h1>';
