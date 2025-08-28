8a37170784c10a7767c21448d7f31ab046b39e1c
Ernest Talagtag <ernestneil09@gmail.com>
2025-08-28 17:51:06 +0800

    improved cart functions

--- cart/script.js
+++ cart/script.js
@@ -1,5 +1,23 @@
 let cartQuantity = 0;
 
 function cartQuantityFunction(){
-    console.log(`Cart Quantity: ${cartQuantity}`);
+    // console.log(`Cart Quantity: ${cartQuantity}`);
+    document.querySelector('.cart-quantity-display').innerHTML = `Cart Quantity: ${cartQuantity}`;
+    document.querySelector('.cat').outerHTML = `<img class="cat" src="cat-meme.png" alt="cat ni ernest" style="display: block;">`;
+}
+// console.log(document.querySelector('.cat'));
+
+function addCart(){
+    (cartQuantity<10) ? cartQuantity++ : console.log('The cart is full');
+    cartQuantityFunction();
+}
+
+function removeCart(){
+    (cartQuantity>0) ? cartQuantity-- : console.log('the cart is empty');
+    cartQuantityFunction();
+}
+
+function clearCart(){
+    cartQuantity = 0;
+    cartQuantityFunction();
 }
